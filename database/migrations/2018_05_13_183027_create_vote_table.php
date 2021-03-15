af79ef35920ffdfb88d68e1f4b25cc455655ba7e
YuliusElfrisaD <yuliuselfrisa897@gmail.com>
2021-03-15 11:00:18 +0700

    Aplikasi KPU

---
+++ database/migrations/2018_05_13_183027_create_vote_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVoteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vote', function (Blueprint $table){
+            $table->unsignedInteger('pemilu_id');
+            $table->unsignedInteger('paslon_id');
+
+            $table->foreign('pemilu_id')
+                    ->references('id')
+                    ->on('pemilu')
+                    ->onDelete('cascade');
+            
+            $table->foreign('paslon_id')
+                    ->references('id')
+                    ->on('paslon')
+                    ->onDelete('cascade');
+
+            $table->primary(['pemilu_id', 'paslon_id']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vote');
+    }
+}
